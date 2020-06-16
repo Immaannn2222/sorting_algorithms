ae804ed14fede693a3dee0dd3bf8eee0a58900f8
Immaannn2222 <imennaayari@gmail.com>
2020-06-16 15:57:18 +0100

    fe

---
+++ 3-quick_sort.c
@@ -0,0 +1,77 @@
+#include "sort.h"
+/**
+ * swap - swaps 2 elements in an array
+ * @f: first element
+ * @s: second element
+ *
+ * Return: void
+ */
+
+void swap(int *f, int *s)
+{
+int tmp;
+tmp = *f;
+*f = *s;
+*s = tmp;
+}
+
+/**
+ * lum_part - lumoto partition for quicksort
+ * @A: array of integers
+ * @lo: the index of the first elt
+ * @hi:starting index(last element)
+ *
+ * Return: i
+ */
+
+int lum_part(int *A, int lo, int hi)
+{
+int i, j;
+int pivot;
+pivot = A[hi];
+i = lo - 1;
+for (j = lo; j <=  hi - 1; j++)
+{
+if (A[j] <= pivot)
+{
+i += 1;
+swap(&A[i], &A[j]);
+}
+}
+swap(&A[i + 1], &A[hi]);
+print_array(A, hi + 1);
+return (i + 1);
+}
+/**
+ * quicksort - quicksorts the array
+ * @A: array of integers
+ * @lo: the index of the first elt
+ * @hi:starting index(last element)
+ *
+ * Return: void
+ */
+int quicksort(int *A, int lo, int hi)
+{
+if (lo < hi)
+{
+int s;
+s = lum_part(A, lo, hi);
+quicksort(A, lo, s - 1);
+quicksort(A, s + 1, hi);
+}
+return (0);
+}
+
+/**
+ * quick_sort - sorts using the Quick sort algorithm
+ * @array: the array
+ * @size: its size
+ *
+ * Return: void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+quicksort(array, 0, size - 1);
+}
+
