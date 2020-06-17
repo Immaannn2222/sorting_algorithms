d571a7d358d5f5dfce125bccb220bdf1cf7376ca
Immaannn2222 <imennaayari@gmail.com>
2020-06-17 22:33:38 +0100

    e

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * cocktail_sort_list - sorts a listin a an ascending order
+ * list: a list
+ *
+ * Return: void
+ */
+
+void cocktail_sort_list(listint_t **list)
+{
+;
+}
